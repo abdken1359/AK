56af4847683ffb2f1f1b6549ac71f6de421b9c12
abdken1359 <abdielkengne6@gmail.com>
2025-03-08 01:42:19 -0400

    May have fixed dark mode issue

--- app.vue
+++ app.vue
@@ -5,27 +5,14 @@
   </div>
 </template>
 <script setup lang="ts">
-console.log(useColorMode())
 
-const colorMode = useColorMode()
-const isDark = computed({
-  get () {
-    return colorMode.value === 'dark'
-  },
-  set () {
-    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
-  }
-});
+
+
 onMounted(()=>{
-  colorMode.value='dark'
-})
-
-
-/*onMounted(()=>{
   useHead({
     htmlAttrs:{
       class:'dark'
     }
   })
-})*/
+})
 </script>
